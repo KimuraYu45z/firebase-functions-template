d27243207c137caed48ef2b9c3e3f2d543a1ad66
unknown <33382781+YuKimura45z@users.noreply.github.com>
2020-02-14 13:11:43 +0900

    [update]

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+
 export * from "./triggers";
+
+export function initialize(
+  initializeApp: (options: {
+    credential: admin.credential.Credential;
+    databaseURL: string;
+  }) => admin.app.App,
+) {
+  const options = {
+    credential: admin.credential.cert(
+      JSON.parse(
+        JSON.stringify(functions.config().service_account).replace(
+          /\\\\n/g,
+          "\\n",
+        ),
+      ),
+    ),
+    databaseURL: functions.config().admin.database_url,
+  };
+  initializeApp(options);
+  admin.initializeApp(options);
+}
